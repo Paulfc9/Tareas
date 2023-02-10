c25098576d4aca076aaf423fb67a4eb32eb05337
ESTRELLA <luzestrellacl302@gmail.com>
2023-02-10 09:21:34 -0600

    first commit

---
+++ CalculadoraEstaciones.js
@@ -0,0 +1,45 @@
+//HECHO POR 23002255 PAUL F. CATALAN LARA
+
+var mes
+mes = prompt("Ingrese el número de un mes: ")
+if(mes >= 1 && mes <= 12){
+  switch(mes){
+    case "1":
+        console.log("Es invierno")
+        break;
+    case "2":
+        console.log("Es invierno")
+        break;
+    case "3":
+        console.log("Es primavera")
+        break;
+    case "4":
+        console.log("Es primavera")
+        break;
+    case "5":
+        console.log("Es primavera")
+        break;
+      case "6":
+        console.log("Es verano")
+        break;
+            case "7":
+        console.log("Es verano")
+        break;
+            case "8":
+        console.log("Es verano")
+        break;
+            case "9":
+        console.log("Es otoño")
+        break;
+            case "10":
+        console.log("Es otoño")
+        break;
+            case "11":
+        console.log("Es otoño")
+        break;
+            case "12":
+        console.log("Es invierno")
+        break;
+  }}  
+       else
+         console.log("Valor invalido")
